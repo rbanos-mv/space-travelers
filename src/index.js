1d64fa3c24214555464ba6e34d5685e12b9ee9a9
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-14 17:01:24 +0200

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
-import { retrieveMissions } from './redux/missions/missions';
+import { getRockets } from './redux/rockets/rockets';
 
-store.dispatch(retrieveMissions());
+store.dispatch(getRockets());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

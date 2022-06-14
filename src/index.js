e128feeff9dff1817e47a30e144ddadc29ff93e5
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-14 04:35:51 -0500

    load rocket data into store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/configureStore';
+import { getRockets } from './redux/rockets/rockets';
+
+store.dispatch(getRockets());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

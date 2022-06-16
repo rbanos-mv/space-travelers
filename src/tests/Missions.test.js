b17782f5570ff2e873ceeefbcc1bd860f27ec3d4
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-16 19:34:26 +0200

    Add test to check if missions renders correctly

--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,5 +1,16 @@
-describe('Test the Missions.test components', () => {
-  test('', () => {
-    expect(true).toBe(true);
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Missions from '../pages/Missions';
+import setupStore from './setupStore';
+
+describe('Test the Missions components', () => {
+  it('renders the Missions page correctly', () => {
+    const store = setupStore(false);
+    const page = render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+    expect(page).toMatchSnapshot();
   });
 });

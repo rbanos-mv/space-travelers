b36f7115837d2d5c25c6d67bb87179a99c7734a2
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-06-16 20:04:04 +0200

    Add tests to join and leave mission buttons

--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Missions from '../pages/Missions';
 import setupStore from './setupStore';
@@ -12,5 +12,63 @@ describe('Test the Missions components', () => {
       </Provider>,
     );
     expect(page).toMatchSnapshot();
+  });
+
+  it('shows the button changes from "Join Mission" to "Leave Mission" on click event', () => {
+    const store = setupStore(false);
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(screen.queryByText('Leave Mission')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Join Mission'));
+    expect(screen.queryByText('Leave Mission')).toBeTruthy();
+    expect(screen.queryByText('Join Mission')).toBeFalsy();
+  });
+
+  it('shows the button changes from "Leave Mission" to "Join Mission" on click event', () => {
+    const store = setupStore(true);
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(screen.queryByText('Join Mission')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Leave Mission'));
+    expect(screen.queryByText('Join Mission')).toBeTruthy();
+    expect(screen.queryByText('Leave Mission')).toBeFalsy();
+  });
+
+  it('shows the active member badge on click event', () => {
+    const store = setupStore(false);
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(screen.queryByText('Active Member')).toBeFalsy();
+    expect(screen.queryByText('NOT A MEMBER')).toBeTruthy();
+    fireEvent.click(screen.queryByText('Join Mission'));
+    expect(screen.queryByText('Active Member')).toBeTruthy();
+    expect(screen.queryByText('NOT A MEMBER')).toBeFalsy();
+  });
+
+  it('hides the active member badge on click event', () => {
+    const store = setupStore(true);
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(screen.queryByText('Active Member')).toBeTruthy();
+    expect(screen.queryByText('NOT A MEMBER')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Leave Mission'));
+    expect(screen.queryByText('Active Member')).toBeFalsy();
+    expect(screen.queryByText('NOT A MEMBER')).toBeTruthy();
   });
 });

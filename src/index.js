fea3dfc7c90e10bdf1a0b9548de80b1417c42c78
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-16 04:10:02 -0500

    Move <Router> from index.js to App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
 import { getRockets } from './redux/rockets/rockets';
@@ -12,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
